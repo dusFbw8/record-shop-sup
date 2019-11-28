835e4ea555602589b9ae677159bbd45144202a56
Jovan Derebanoski <jd7@students.kiron.ngo>
2019-11-28 12:22:09 +0100

    add gitignore file

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-// app.use(corsMiddleWare)
+app.use(corsMiddleWare)
 
 
 
